f9dfcec510becb6ca69b7b2f371795213d17c319
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-05 14:42:27 +0300

    Initial Commit

---
+++ TwitterSplashScreen_SwiftUI/TwitterSplashScreen_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSplashScreen_SwiftUIApp.swift
+//  TwitterSplashScreen_SwiftUI
+//
+//  Created by Sade on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSplashScreen_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
